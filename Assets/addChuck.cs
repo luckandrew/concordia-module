d9375eb84cd619f2b835180a84ba05fe35b1a688
Andrew Luck <contact@andrewluck.com>
2019-01-19 18:26:49 -0500

    Adding Chuck Sound
    Adding Chuck Sound

---
+++ Assets/addChuck.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addChuck : MonoBehaviour {
+
+    // Use this for initialization
+    void Start()
+    {
+
+        GetComponent<ChuckSubInstance>().RunCode(@"
+        SinOsc foo => dac;
+        while( true )
+        {
+            Math.random2f( 300, 1000 ) => foo.freq;
+            100::ms => now;
+        }
+    ");
+    }
+}
